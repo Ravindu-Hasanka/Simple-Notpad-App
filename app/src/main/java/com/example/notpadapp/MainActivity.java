3a969ab51db497ab8ef9ec4eaa776107b05f627a
Ravindu Hasanka <rhatu2000@gmail.com>
2024-08-08 01:42:18 +0530

    main activity

--- app/src/main/java/com/example/notpadapp/MainActivity.java
+++ app/src/main/java/com/example/notpadapp/MainActivity.java
@@ -2,13 +2,80 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+    public class MainActivity extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "NotesPrefs";
+    private static final String KEY_NOTE_COUNT = "NoteCount";
+
+    private LinearLayout notesContainer;
+    private List<Note> notes;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notesContainer = findViewById(R.id.notesLayout);
+        Button addNoteButton = findViewById(R.id.addNoteButton);
+
+        notes = new ArrayList<>();
+
+        addNoteButton.setOnClickListener(v -> {
+            saveNote();
+        });
     }
-}
+
+    private void saveNote() {
+        EditText titleEditText = findViewById(R.id.titleEditText);
+        EditText contentEditText = findViewById(R.id.descriptionEditText);
+
+        String title = titleEditText.getText().toString();
+        String content = contentEditText.getText().toString();
+
+        if(title.isEmpty() || content.isEmpty()) {
+            return;
+        }
+
+        Note note = new Note();
+        note.setTitle(title);
+        note.setContent(content);
+        Date date = new Date();
+        note.setDate(date.toString());
+
+        notes.add(note);
+        saveNotesToPreferences();
+
+        createNotesView();
+    }
+
+        private void createNotesView() {
+            View noteView = getLayoutInflater().inflate(R.layout.note_item.xml);
+        }
+
+        private void saveNotesToPreferences() {
+            SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+
+            editor.putInt(KEY_NOTE_COUNT, notes.size());
+            for(int i = 0; i < notes.size(); i++) {
+                Note note = notes.get(i);
+                editor.putString("noteTitle" + i, note.getTitle());
+                editor.putString("noteContent" + i, note.getContent());
+                editor.putString("noteDate" + i, note.getDate());
+            }
+            editor.apply();
+        }
+    }
---
+++ app/src/main/java/com/example/notpadapp/Note.java
@@ -0,0 +1,40 @@
+package com.example.notpadapp;
+
+public class Note {
+    private String title;
+    private String content;
+    private String date;
+
+    public Note() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    Note (String title, String content, String date) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+    }
+}
